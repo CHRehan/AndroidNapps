b820186d33c90bf43a2aededc487609d61343a1c
Rehan Sarwar <rehan.sarwar01@confiz.com>
2022-02-22 11:29:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/androidnapps/HttpClientResponse.java
@@ -0,0 +1,14 @@
+package com.example.androidnapps;
+
+
+public class HttpClientResponse {
+
+    public int responseCode;
+    public String responseData;
+
+    public HttpClientResponse() {
+        responseCode = 0;
+        responseData = null;
+    }
+
+}
